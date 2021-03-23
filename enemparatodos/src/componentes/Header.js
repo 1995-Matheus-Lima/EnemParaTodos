7655f40bb6ca874f988d48bd99484c68d94965f5
1995-Matheus-Lima <71905614+1995-Matheus-Lima@users.noreply.github.com>
2021-03-22 22:04:20 -0300

    criado o protótipo do componente Header

---
+++ enemparatodos/src/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+const Header = () => {
+  return (
+    <div className="HeaderMenu">
+      <h1>Enem para todos</h1>
+    </div>
+  )
+}
+
+export default Header
